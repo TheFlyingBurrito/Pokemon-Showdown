cca21156b74cdcf9b346818da9204768c618d72e
Chinlar <aluis.basketball@hotmail.com>
2013-07-18 21:28:45 -0400

    Update commands.js

--- config/commands.js
+++ config/commands.js
@@ -749,7 +749,11 @@ var commands = exports.commands = {
 		if (!this.canBroadcast()) return;
 		return this.sendReplyBox('Parukia Announcements:<br><br>Sign up for the Official Social Group of Parukia <a href="http://forums.parukia.net/showthread.php/2116-Parukia-s-Social-Group-Recruitment?p=21115#post21115">here!</a><br><br>Sign up for our RU Tournament <a href="http://forums.parukia.net/showthread.php/2181-Parukia-RU-Tourney?p=22166#post22166">here!</a><br><br>Sign up for our massive summer OU Tournament <a href="http://forums.parukia.net/showthread.php/2240-Massive-Summer-OU-Tournament-Signups!">here!</a>');
 	},
-
+	
+	pew: function(target, room, user) {
+		if (!this.canBroadcast()) return;
+		return this.sendReplyBox('Pew');
+	}, 
 	/*********************************************************
 	 * Miscellaneous commands
 	 *********************************************************/
