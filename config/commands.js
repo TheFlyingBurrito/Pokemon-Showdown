6652ada408697ba630d16ca4aba4befbff0c6e2e
Chinlar <aluis.basketball@hotmail.com>
2013-08-17 22:07:24 -0400

    Update commands.js

--- config/commands.js
+++ config/commands.js
@@ -985,8 +985,6 @@ var commands = exports.commands = {
 		this.sendReplyBox('<b><font col<b><font color="green">FRONTIER BRAINS</font></b><br>Ubers: XLR8R<br>OU: EricSaysHi<br>UU: The Flying Burrito<br>RU: Chinlar<br>NU: NU King Gary<br>Balanced Hackmons: Gamebrεaker<br><br>' +or="purple">NU King Gary</font><br><br><font color="purple">Wins / Losses</font><br><br><font color="purple">0 Wins - 0 Losses</font><br><br><font color="purple">NU Frontier Rules</font><br><br><font color="purple">Coming soon!</font></b>');
 	},
 	
-	brainlist: 'brain',
-	brains: 'brain',
 	brain: function(target, room, user) {
 		if (!this.canBroadcast()) return;
 		this.sendReplyBox('<b><font color="green">FRONTIER BRAINS</font></b><br>Ubers: XLR8R<br>OU: EricSaysHi<br>UU: The Flying Burrito<br>RU: Chinlar<br>NU: NU King Gary<br>Balanced Hackmons: Gamebrεaker');
