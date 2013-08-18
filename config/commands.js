5253eb0272c5ebb5aafefa4dd9fcb143248f60bb
Parukia <dragontrainerryan724@yahoo.com>
2013-08-17 17:08:55 -0700

    Update commands.js

--- config/commands.js
+++ config/commands.js
@@ -980,7 +980,7 @@ var commands = exports.commands = {
 	
 	nu_frontier: function(target, room, user) {
 		if (!this.canBroadcast()) return;
-		this.sendReplyBox('<b><font color="purple">NU King Gary</font></b><br><br><font color="purple">Wins / Losses</font><br><br><font color="purple">0 Wins - 0 Losses</font><br><br><font color="purple">NU Frontier Rules</font><br><br><font color="purple">Coming soon!</font>');
+		this.sendReplyBox('<b><font color="purple">NU King Gary</font><br><br><font color="purple">Wins / Losses</font><br><br><font color="purple">0 Wins - 0 Losses</font><br><br><font color="purple">NU Frontier Rules</font><br><br><font color="purple">Coming soon!</font></b>');
 	},
 	
 	/*********************************************************
