cb55fb27abbc19b65bc35e2de44e486bb22383ab
Chinlar <aluis.basketball@hotmail.com>
2013-08-19 20:20:18 -0400

    Update commands.js

--- config/commands.js
+++ config/commands.js
@@ -998,7 +998,7 @@ var commands = exports.commands = {
 		var matched = false;
 		if (target === ''){
 			matched = true;
-			this.sendReplyBox('<b><font color="green">FRONTIER BRAINS</font></b><br><br>Ubers: XLR8R<br>OU: EricSaysHi<br>UU: The Flying Burrito<br>RU: Chinlar<br>NU: NU King Gary<br>Balanced Hackmons: Gamebrεaker<br><br><b><font color="purple">Do /brain [name] like /brain Chinlar to see their rules.</font>');
+			this.sendReplyBox('<b><font color="green">FRONTIER BRAINS</font></b><br><br>Ubers: XLR8R<br>OU: EricSaysHi<br>UU: The Flying Burrito<br>RU: Chinlar<br>NU: NU King Gary<br>Balanced Hackmons: Gamebrεaker<br><br><b><font color="purple">Do /brain [name] such as /brain Chinlar to see their rules.</font>');
                 }
 		if (target === 'chinlar'){
 			matched = true;
@@ -1010,7 +1010,7 @@ var commands = exports.commands = {
 		}
 		if (target === 'gamebreaker'){
 			matched = true;
-			this.sendReplyBox('<b>Gamebreaker</b><br><br><b>Tier: BH</b><br><br><img src="http://i.imgur.com/M6PI4nR.png"><br><br><b>Wins / Losses</b><br> 0 Wins - 0 Losses<br><br><b>Frontier Rules</b><br>- Water Type Banned<br>- Batton Pass Banned');
+			this.sendReplyBox('<b>Gamebreaker</b><br><br><b>Tier: Balanced Hackmons</b><br><br><img src="http://i.imgur.com/M6PI4nR.png"><br><br><b>Wins / Losses</b><br> 0 Wins - 0 Losses<br><br><b>Frontier Rules</b><br>- Water Type Banned<br>- Batton Pass Banned');
 		}
 		if (target === 'xlr8r'){
 			matched = true;
