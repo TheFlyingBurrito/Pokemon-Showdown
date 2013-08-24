7406980a767f7906cef6d3e99044a2e6fec69cd4
Chinlar <aluis.basketball@hotmail.com>
2013-08-24 11:57:51 -0400

    Update tour.js

--- tour.js
+++ tour.js
@@ -1000,7 +1000,7 @@ var cmds = {
 		var answers = answers.join(',').toLowerCase().split(',');
 		tour[room.id].question = question;
 		tour[room.id].answerList = answers;
-		room.addRaw('<div class="infobox"><h2>' + tour[room.id].question + separacion + '<font class="closebutton" size=1><small>/vote OPTION</small></font><br><br><font size="1"><i>' + sanitize(user.name) + ' started the poll.</i></font></h2><hr />' + separacion + separacion + " &bull; " + tour[room.id].answerList.join(' &bull; ') + '</div>');
+		room.addRaw('<div class="infobox"><h2>' + tour[room.id].question + separacion + '<font class="closebutton" size=1><small>/vote OPTION</small></font></h2><br><font size="1"><i>' + sanitize(user.name) + ' started the poll.</i></font><hr />' + separacion + separacion + " &bull; " + tour[room.id].answerList.join(' &bull; ') + '</div>');
 	},
 	
 	vote: function(target, room, user) {
