408efedfeed454273b18ac1d976a00a87f9c4561
Chinlar <aluis.basketball@hotmail.com>
2014-03-31 21:30:08 -0300

    Update users.js

--- users.js
+++ users.js
@@ -1513,6 +1513,8 @@ exports.socketDisconnect = socketDisconnect;
 exports.socketReceive = socketReceive;
 
 exports.importUsergroups = importUsergroups;
+exports.importUseravatars = importUseravatars;
+exports.exportUseravatars = exportUseravatars;
 exports.addBannedWord = addBannedWord;
 exports.removeBannedWord = removeBannedWord;
 
@@ -1523,6 +1525,7 @@ exports.bannedIps = bannedIps;
 exports.lockedIps = lockedIps;
 
 exports.usergroups = usergroups;
+exports.useravatars = useravatars;
 
 exports.pruneInactive = User.pruneInactive;
 exports.pruneInactiveTimer = setInterval(
