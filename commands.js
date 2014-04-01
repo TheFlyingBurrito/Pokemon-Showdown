568af637cd7879f38d64d0685f272b9244fdbcf0
Chinlar <aluis.basketball@hotmail.com>
2014-04-01 20:17:21 -0300

    Update commands.js

--- commands.js
+++ commands.js
@@ -1286,19 +1286,19 @@ var commands = exports.commands = {
 		target = this.splitTarget(target);
 		var targetUser = this.targetUser;
 		if (!targetUser) {
-			return this.sendReply('User '+this.targetUsername+' not found.');
+			return this.sendReply("User " + this.targetUsername + " not found.");
 		}
 		if (!target) {
-			return this.sendReply('No new name was specified.');
+			return this.sendReply("No new name was specified.");
 		}
 		if (!this.can('forcerenameto', targetUser)) return false;
 
 		if (targetUser.userid === toUserid(this.targetUser)) {
-			var entry = ''+targetUser.name+' was forcibly renamed to '+target+' by '+user.name+'.';
-			this.privateModCommand('(' + entry + ')');
-			targetUser.forceRename(target, undefined, true);
+			var entry = targetUser.name + " was forcibly renamed to " + target + " by " + user.name + ".";
+			this.privateModCommand("(" + entry + ")");
+			targetUser.forceRename(target, null, true);
 		} else {
-			this.sendReply("User "+targetUser.name+" is no longer using that name.");
+			this.sendReply("User " + targetUser.name + " is no longer using that name.");
 		}
 	},
 
